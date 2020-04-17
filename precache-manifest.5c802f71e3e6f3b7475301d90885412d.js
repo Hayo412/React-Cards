3e54f86f36653d62413a267f4dd4bd16c7ea387b
hibarra <hibarra@guaostudio.com>
2020-04-16 20:59:28 -0500

    Updates

---
+++ precache-manifest.5c802f71e3e6f3b7475301d90885412d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165238e88fd92b549f610ae73f74a81d",
+    "url": "/tarjetones/index.html"
+  },
+  {
+    "revision": "d6b58ca43314a8c035ca",
+    "url": "/tarjetones/static/css/main.16662801.chunk.css"
+  },
+  {
+    "revision": "7aabcabdcfb2b6fb4792",
+    "url": "/tarjetones/static/js/2.7bc78169.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarjetones/static/js/2.7bc78169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b58ca43314a8c035ca",
+    "url": "/tarjetones/static/js/main.b163531d.chunk.js"
+  },
+  {
+    "revision": "e05cdbd49d420fc6c4fe",
+    "url": "/tarjetones/static/js/runtime-main.e1872f37.js"
+  }
+]);
